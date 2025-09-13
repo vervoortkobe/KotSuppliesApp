ee9b173121f933f7e5af55bbc379e6fdc6a93469
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2025-09-14 00:20:35 +0200

    Add images

--- backend/src/images/images.service.ts
+++ backend/src/images/images.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Image } from '../entities/image.entity';
 
 @Injectable()
-export class ImagesService {}
+export class ImagesService {
+  constructor(
+    @InjectRepository(Image) private imageRepository: Repository<Image>,
+  ) {}
+
+  async upload(file: Express.Multer.File) {
+    const image = new Image();
+    image.data = file.buffer;
+    image.mimeType = file.mimetype;
+    return this.imageRepository.save(image);
+  }
+
+  async findOne(guid: string) {
+    return this.imageRepository.findOneBy({ guid });
+  }
+}
