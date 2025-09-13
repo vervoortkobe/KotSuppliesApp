68c638bb58893f5b542e07ce8d64b54bf626e1ed
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2025-09-14 00:19:30 +0200

    Add categories

--- backend/src/categories/categories.controller.ts
+++ backend/src/categories/categories.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Put,
+  Delete,
+  BadRequestException,
+} from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto, UpdateCategoryDto } from './dto';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Post(':listGuid')
+  async create(
+    @Param('listGuid') listGuid: string,
+    @Body() createCategoryDto: CreateCategoryDto,
+  ) {
+    return this.categoriesService.create(listGuid, createCategoryDto);
+  }
+
+  @Get(':listGuid/:categoryGuid')
+  async findOne(
+    @Param('listGuid') listGuid: string,
+    @Param('categoryGuid') categoryGuid: string,
+  ) {
+    const category = await this.categoriesService.findOne(
+      listGuid,
+      categoryGuid,
+    );
+    if (!category) {
+      throw new BadRequestException('Category not found');
+    }
+    return category;
+  }
+
+  @Put(':listGuid/:categoryGuid')
+  async update(
+    @Param('listGuid') listGuid: string,
+    @Param('categoryGuid') categoryGuid: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
+    return this.categoriesService.update(
+      listGuid,
+      categoryGuid,
+      updateCategoryDto,
+    );
+  }
+
+  @Delete(':listGuid/:categoryGuid')
+  async delete(
+    @Param('listGuid') listGuid: string,
+    @Param('categoryGuid') categoryGuid: string,
+  ) {
+    return this.categoriesService.delete(listGuid, categoryGuid);
+  }
+}
