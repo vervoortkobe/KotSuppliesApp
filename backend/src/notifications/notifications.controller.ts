750211fde27e36709a78493a1168e3ce9cbf1369
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2025-09-14 00:21:13 +0200

    Add notifications

--- backend/src/notifications/notifications.controller.ts
+++ backend/src/notifications/notifications.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, BadRequestException } from '@nestjs/common';
+import { NotificationsService } from './notifications.service';
 
 @Controller('notifications')
-export class NotificationsController {}
+export class NotificationsController {
+  constructor(private readonly notificationsService: NotificationsService) {}
+
+  @Get(':userGuid')
+  async findAll(@Param('userGuid') userGuid: string) {
+    const notifications = await this.notificationsService.findAll(userGuid);
+    if (!notifications) {
+      throw new BadRequestException('User not found');
+    }
+    return notifications;
+  }
+}
--- backend/src/notifications/notifications.service.ts
+++ backend/src/notifications/notifications.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Notification } from '../entities/notification.entity';
+import { User } from '../entities/user.entity';
+import { List } from '../entities/list.entity';
 
 @Injectable()
-export class NotificationsService {}
+export class NotificationsService {
+  constructor(
+    @InjectRepository(Notification)
+    private notificationRepository: Repository<Notification>,
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async findAll(userGuid: string) {
+    return this.notificationRepository.find({
+      where: { user: { guid: userGuid } },
+      relations: ['list'],
+      order: { createdAt: 'DESC' },
+    });
+  }
+
+  async notifyUsers(list: List, excludeUser: User | null, message: string) {
+    const notifications = list.users
+      .filter((user) => !excludeUser || user.guid !== excludeUser.guid)
+      .map((user) => {
+        const notification = new Notification();
+        notification.message = message;
+        notification.user = user;
+        notification.list = list;
+        return notification;
+      });
+    await this.notificationRepository.save(notifications);
+  }
+}
