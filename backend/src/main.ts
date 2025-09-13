e51de465f60297a2736b22261c5d339236a2c907
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2025-09-14 00:22:01 +0200

    Add multer

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(
+    MulterModule.register({
+      storage: memoryStorage(),
+    }),
+  );
+  await app.listen(3000);
 }
 bootstrap();
