4cfa91ffb3ee70bf41aeea15f2a651bd18ad65ea
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2025-09-25 12:45:07 +0200

    bugfix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,6 +9,6 @@ import 'dotenv/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
